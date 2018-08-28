82929d28ffefb150728fc975903902e88b48908c
naragudipati <narasimharao.gudipati@nttdata.com>
2018-08-28 15:26:51 -0600

    multiple jars

--- components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
+++ components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
@@ -1,6 +1,6 @@
 package org.superbiz.moviefun.albums;
 
-import org.apache.tika.io.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
